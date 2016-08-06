aa75c8d718bd0813699a9ebf36a679d4ceabcf4d
Wei Jun <junster90@gmail.com>
2016-08-06 19:33:41 +0800

    added solution

---
+++ solution.rb
@@ -0,0 +1,36 @@
+def solution(a)
+  daily_ticket = 2
+  weekly_ticket = 7
+  monthly_ticket = 25
+
+  cost = 0
+
+  return 25 if a.length >= 25
+
+  until a.length == 0 do
+    today = a.first
+
+    end_of_week = today + 6
+    week_utilization = a.select {|x| x <= end_of_week }
+
+    better_op_exists = false
+
+    week_utilization.each do |day|
+      last_day = day + 6
+      potential = a.select {|x| x >= day && x <= last_day}
+      if potential.length > week_utilization.length
+        better_op_exists =  true
+      end
+    end
+
+    if week_utilization.length >= 4 && !better_op_exists
+      cost += weekly_ticket
+      a.reject! { |x| x <= end_of_week}
+    else
+      cost += daily_ticket
+      a.delete(today)
+    end
+  end
+
+  cost
+end
